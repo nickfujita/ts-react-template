ae4532c6473e7be5def3a88d1292cd09562f86d8
nicolasfujita <nickfujita@gmail.com>
2017-10-24 21:16:53 +0900

    cleanup the routes file
    Jira:
    Reviewer:
    PR: GH-

--- src/scripts/main.tsx
+++ src/scripts/main.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import configureStore from './store';
-import { getRoutes } from './routes';
 
 import { Route, IndexRoute } from 'react-router';
 import App from './pages/app';
